a14490d6d7c78c2e1853dad05a5042d6f639fb33
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-05-29 23:02:53 +0200

    token

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function auth(req, res, next) {
-    const token = req.header('auth-token');
+module.exports = async function auth(req, res, next) {
+    const token = await req.header('auth-token');
     if (!token) return res.status(401).send('Access Denied');
 
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verified;
+        const verified = await jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = await verified;
         next();
     } catch (err) {
         res.status(400).send('Invalid Token');
